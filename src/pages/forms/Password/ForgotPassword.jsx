592b64d9abf495d77aa2d566f55960e832062e17
Azarges <azarges@hotmail.fr>
2025-06-17 11:16:20 +0200

    ForgotPassword.jsx fait

--- src/pages/forms/Password/ForgotPassword.jsx
+++ src/pages/forms/Password/ForgotPassword.jsx
@@ -4,16 +4,18 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { forgotPassword } from "../../../apis/auth.api";
 import toast from "react-hot-toast";
 import logo from "../../../assets/logo.png";
+import Button from "../../../components/shared/button";
+import { NavLink } from "react-router-dom";
 
 export default function ForgotPassword() {
   const schema = yup.object({
     email: yup
       .string()
-      .email("Format non valide")
-      .required("Le champ est obligatoire")
+      .email()
+      .required("Veuillez entrer votre adresse e-mail.")
       .matches(
         /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-        "Format de votre email non valide"
+        "Format de votre email non valide."
       ),
   });
 
@@ -29,7 +31,7 @@ export default function ForgotPassword() {
   } = useForm({
     defaultValues,
     resolver: yupResolver(schema),
-    mode: "onChange",
+    mode: "onSubmit",
   });
 
   async function submit(values) {
@@ -39,40 +41,52 @@ export default function ForgotPassword() {
     toast.success(response.message);
   }
   return (
-    <div className="flex items-center justify-center flex-1">
-      <img
-        src={logo}
-        alt="Logo"
-        className="w-25 h-25 max-sm:w-19 max-sm:h-19"
-      />
+    <div className="flex flex-col items-center justify-center flex-1 max-sm:px-5 max-sm:py-7.5 gap-2.5">
+      <div className="flex flex-col gap-2.5 py-12 justify-center items-center">
+        <img
+          src={logo}
+          alt="Logo"
+          className="w-25 h-25 max-sm:w-19 max-sm:h-19"
+        />
 
-      <h2 className="font-title text-[40px] leading-[52px] text-gold font-semibold max-sm:text-[26px] max-sm:leading-[34px]">
-        Connexion
-      </h2>
-      <div className="w-full max-w-md p-6 bg-white rounded shadow-xl">
-        <form
-          onSubmit={handleSubmit(submit)}
-          className="flex flex-col gap-4 mb-6 mx-auto max-w-[400px]"
-        >
-          <div className="flex flex-col mb-2">
-            <label htmlFor="email" className="mb-2">
-              Email
-            </label>
-            <input
-              {...register("email")}
-              type="email"
-              id="email"
-              className="px-3 py-2 border border-gray-300 rounded outline-none focus: focus:ring-2 focus:ring-blue-500"
-            />
-            {errors.email && (
-              <p className="text-red-500">{errors.email.message}</p>
-            )}
-          </div>
-          <button className="px-4 py-2 text-white bg-blue-500 rounded cursor-pointer hover:bg-blue-600">
-            Envoyer
-          </button>
-        </form>
+        <h2 className="font-title text-[40px] leading-[52px] text-gold font-semibold max-sm:text-[26px] max-sm:leading-[34px]">
+          Mot de passe oublié ?
+        </h2>
       </div>
+      <form
+        onSubmit={handleSubmit(submit)}
+        className="flex flex-col gap-2.5 items-center"
+      >
+        <div className="flex flex-col gap-2.5 p-2.5">
+          <label
+            htmlFor="email"
+            className="text-white font-title text-[28px] leading-[37px] max-sm:text-[18px] max-sm:leading-[23px]"
+          >
+            Adresse e-mail
+          </label>
+          <input
+            {...register("email")}
+            type="email"
+            id="email"
+            className="h-[35px] w-75 rounded-[5px] bg-bg-input border-1 border-borderGold p-2.5 text-white max-sm:w-auto"
+          />
+          {errors.email && (
+            <p className="text-light-red text-[14px] max-sm:text-xs leading-[18px] max-sm:leading-[13px]">
+              {errors.email.message}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col text-white leading-[19px] max-sm:text-[14px] max-sm:leading-[17px] p-2.5">
+          <p>Vous avez besoin d'aide supplémentaire ?</p>
+          <p>
+            Contactez nous via notre{" "}
+            <NavLink to="/" className="underline">
+              système de support
+            </NavLink>
+          </p>
+        </div>
+        <Button txt="Démarrer" />
+      </form>
     </div>
   );
 }
